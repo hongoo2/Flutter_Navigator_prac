bcadd13f2cdb837cea9fcfffc82e1acc32fee897
hongu <namhongui0312@gmail.com>
2023-09-03 18:10:31 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+void main()=> runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHome(),
+    );
+  }
+}
+
+class MyHome extends StatelessWidget {
+  const MyHome({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context2) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('First page'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: Text('Go to Second page'),
+          onPressed: (){
+            Navigator.push(context2, MaterialPageRoute(
+              builder: (BuildContext context){
+                return SecondPage();
+              }
+            ));
+          },
+        ),
+      ),
+
+    );
+  }
+}
+
+class SecondPage extends StatelessWidget {
+  const SecondPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext ctx) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Second page'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: Text('Go to First page'),
+          onPressed: (){
+            Navigator.pop(ctx);
+          },
+        ),
+      ),
+    );
+  }
+}
+
+
